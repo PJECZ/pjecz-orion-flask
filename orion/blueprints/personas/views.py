8fcb6f7e261bc1d50f994bd2457b7f05f434e71d
Guillermo Valdes <guillermo@movimientolibre.com>
2024-08-14 10:39:41 -0600

    Ya funciona personas

---
+++ orion/blueprints/personas/views.py
@@ -0,0 +1,99 @@
+"""
+Personas, vistas
+"""
+
+import json
+
+from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_required
+
+from lib.datatables import get_datatable_parameters, output_datatable_json
+from lib.safe_string import safe_message, safe_string
+from orion.blueprints.bitacoras.models import Bitacora
+from orion.blueprints.modulos.models import Modulo
+from orion.blueprints.permisos.models import Permiso
+from orion.blueprints.personas.models import Persona
+from orion.blueprints.usuarios.decorators import permission_required
+
+MODULO = "PERSONAS"
+
+personas = Blueprint("personas", __name__, template_folder="templates")
+
+
+@personas.before_request
+@login_required
+@permission_required(MODULO, Permiso.VER)
+def before_request():
+    """Permiso por defecto"""
+
+
+@personas.route("/personas/datatable_json", methods=["GET", "POST"])
+def datatable_json():
+    """DataTable JSON para listado de Personas"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = Persona.query
+    # Primero filtrar por columnas propias
+    if "estatus" in request.form:
+        consulta = consulta.filter_by(estatus=request.form["estatus"])
+    else:
+        consulta = consulta.filter_by(estatus="A")
+    if "numero_empleado" in request.form:
+        try:
+            numero_empleado = int(request.form["numero_empleado"])
+            consulta = consulta.filter_by(numero_empleado=numero_empleado)
+        except ValueError:
+            pass
+    # Luego filtrar por columnas de otras tablas
+    # if "persona_rfc" in request.form:
+    #     consulta = consulta.join(Persona)
+    #     consulta = consulta.filter(Persona.rfc.contains(safe_rfc(request.form["persona_rfc"], search_fragment=True)))
+    # Ordenar y paginar
+    registros = consulta.order_by(Persona.id).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for resultado in registros:
+        data.append(
+            {
+                "numero_empleado": resultado.numero_empleado,
+                "detalle": {
+                    "nombre_completo": resultado.nombre_completo,
+                    "url": url_for("personas.detail", persona_id=resultado.id),
+                },
+                "situacion": resultado.situacion,
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@personas.route("/personas")
+def list_active():
+    """Listado de Personas activos"""
+    return render_template(
+        "personas/list.jinja2",
+        filtros=json.dumps({"estatus": "A"}),
+        titulo="Personas",
+        estatus="A",
+    )
+
+
+@personas.route("/personas/inactivos")
+@permission_required(MODULO, Permiso.ADMINISTRAR)
+def list_inactive():
+    """Listado de Personas inactivos"""
+    return render_template(
+        "personas/list.jinja2",
+        filtros=json.dumps({"estatus": "B"}),
+        titulo="Personas inactivos",
+        estatus="B",
+    )
+
+
+@personas.route("/personas/<int:persona_id>")
+def detail(persona_id):
+    """Detalle de un Persona"""
+    persona = Persona.query.get_or_404(persona_id)
+    return render_template("personas/detail.jinja2", persona=persona)
