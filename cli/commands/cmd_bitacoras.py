964299182753b08a144f36838a5e11859b8b7ee2
ricval <rcvaldes@gmail.com>
2025-05-07 12:13:22 -0600

    Añadido CLI para enviar reportes de la bitácora

---
+++ cli/commands/cmd_bitacoras.py
@@ -0,0 +1,36 @@
+"""
+CLI Bitácoras
+"""
+
+import sys
+
+import click
+
+from orion.blueprints.bitacoras.tasks import enviar_reporte_diario
+from lib.exceptions import MyAnyError
+
+
+@click.group()
+def cli():
+    """Bitácoras"""
+
+
+@click.command()
+@click.argument("modulo_nombre", type=str)
+@click.argument("to_email", type=str)
+def enviar(modulo_nombre, to_email):
+    """Enviar mensaje con el reporte diario del módulo dado"""
+    click.echo(f"Inicia el envío del mensaje con el reporte diario del módulo {modulo_nombre} a {to_email}")
+
+    # Enviar el mensaje
+    try:
+        mensaje_termino = enviar_reporte_diario(modulo_nombre, to_email)
+    except MyAnyError as error:
+        click.echo(error)
+        sys.exit(1)
+
+    # Mensaje de termino
+    click.echo(mensaje_termino)
+
+
+cli.add_command(enviar)
